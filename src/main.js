71202a7df2fe2ec7658bd412ac73b5140800dcdc
AlexScherbackov <alsherbakov13@gmail.com>
2019-03-31 20:47:29 +0400

    added font compilation to config

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import axios from 'axios';
+import store from './store/store.js';
+import ElementUI from 'element-ui';
+import locale from 'element-ui/lib/locale/lang/en';
+
+import 'element-ui/lib/theme-chalk/index.css';
+
+window.axios = axios;
+Vue.use(ElementUI, { locale });
 
 new Vue({
+	store,
   el: '#app',
   render: h => h(App)
 })
